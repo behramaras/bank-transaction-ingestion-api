d6d8ccfdbe4449935e4c1204f7d4d2365141ea47
Behram Aras <behram.aras@gmail.com>
2026-03-17 07:47:33 +0000

    feat: add UploadResponse schema

--- app/helpers.py
+++ app/helpers.py
@@ -1,2 +1,9 @@
 from pydantic import BaseModel
 from typing import Optional
+
+class UploadResponse(BaseModel):
+    upload_id : str
+    status : str
+    rows_ingested : Optional[int] = None
+    rows_failed : int
+    failure_report_url : Optional[str] = None
