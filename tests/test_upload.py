2c30356eca0b382a4d76be03963ed1011af4b2c9
Behram Aras <behram.aras@gmail.com>
2026-03-23 07:22:00 +0000

    feat(tests): add test for failure report endpoint response

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -62,13 +62,13 @@ def test_invalid_csv_success():
     }
 
     def test_exceding_threshold():
-    transaction_id1 = str(uuid.uuid4())
-    transaction_id2 = str(uuid.uuid4())
-    transaction_id3 = str(uuid.uuid4())
-    test_csv = f"""transaction_id,account_id,user_id,timestamp,amount,currency,merchant_id,category
-{transaction_id1},ACC-0012,USR-0003,2024-01-15T09:23:11Z,-42.50,GBPT,MRC-0042,groceries
-{transaction_id2},ACC-0012,USR-0003,2024-01-16T14:05:00Z,1850.00,GBP,MRC-0017,bincome
-{transaction_id3},ACC-0012,USR-0003,2024-01-16T14:05:00Z,1850.00,GBP,MRC-0017,groceriesx"""
+        transaction_id1 = str(uuid.uuid4())
+        transaction_id2 = str(uuid.uuid4())
+        transaction_id3 = str(uuid.uuid4())
+        test_csv = f"""transaction_id,account_id,user_id,timestamp,amount,currency,merchant_id,category
+    {transaction_id1},ACC-0012,USR-0003,2024-01-15T09:23:11Z,-42.50,GBPT,MRC-0042,groceries
+    {transaction_id2},ACC-0012,USR-0003,2024-01-16T14:05:00Z,1850.00,GBP,MRC-0017,bincome
+    {transaction_id3},ACC-0012,USR-0003,2024-01-16T14:05:00Z,1850.00,GBP,MRC-0017,groceriesx"""
     
     response = client.post(
         "/transactions/upload",
@@ -77,3 +77,20 @@ def test_invalid_csv_success():
     
     assert response.status_code == 422
     assert response.json()["detail"]["status"] == "aborted"
+
+def test_failure_endpoint_returns_200():
+    transaction_id1 = str(uuid.uuid4())
+    test_csv = f"""transaction_id,account_id,user_id,timestamp,amount,currency,merchant_id,category
+{transaction_id1},ACC-0012,USR-0003,2024-01-15T09:23:11Z,-42.50,GBPT,MRC-0042,groceries"""
+
+    upload_response = client.post(
+       "/transactions/upload",
+        files = {"file": ("test.csv", test_csv, "text/csv")})
+       
+    upload_id = upload_response.json()["upload_id"]
+
+    failure_response = client.get(
+       f"/transactions/upload/{upload_id}/failures"
+    )
+    
+    assert failure_response.status_code == 200
